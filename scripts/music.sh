bdb263d9369d9a56a4b3715d2098a0baa1a0a172
James Vaughan <james@jamesbvaughan.com>
2016-03-19 19:39:42 -0700

    added music script

--- scripts/music.sh
+++ scripts/music.sh
@@ -1,1 +1,13 @@
-.bin/music.sh
+#!/bin/sh
+
+if [ "$BLOCK_BUTTON" = 1 ];then
+	mpc toggle -q
+	mpc current | tr -d "\n"
+else
+	mpc status | perl -ne 'if (/\[playing\]/) {CORE::say (`mpc current | tr -d "\n"`)}'
+fi
+
+#STATUS=$()
+# echo $STATUS
+
+
