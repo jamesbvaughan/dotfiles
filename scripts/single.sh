d1b5ee56a2bc90489680001a18c88ae83e07fdd9
James Vaughan <james@jamesbvaughan.com>
2017-01-17 07:12:14 -0800

    updates

--- scripts/single.sh
+++ scripts/single.sh
@@ -3,4 +3,8 @@
 # Disable a second monitor
 xrandr --output HDMI1 --off
 xrandr --output DP1 --off
-echo "Second monitor disabled"
+
+# Map the touchscreen to the laptop screen
+xinput --map-to-output 11 eDP1
+
+notify-send "Second monitor disabled"
