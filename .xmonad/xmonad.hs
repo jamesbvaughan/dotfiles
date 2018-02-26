a7b1669a03d6cd73213f97c2604feddb9404ff67
James Vaughan <james@jamesbvaughan.com>
2018-02-25 17:27:51 -0800

    removed borders and simplified config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,19 +1,9 @@
 import XMonad
 import XMonad.Actions.CycleWS
 import XMonad.Config.Desktop
-import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Fullscreen
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
-
-
-myLayoutHook = tall ||| wide ||| full
-  where
-    tall = smartBorders $ smartSpacingWithEdge 0 $ Tall 1 0.03 0.5
-    wide = Mirror tall
-    full = noBorders Full
 
 
 main = do
@@ -23,15 +13,9 @@ main = do
 
     , workspaces = ["web", "term", "music", "3", "4"]
 
-    , layoutHook = lessBorders OnlyFloat $ avoidStruts $ myLayoutHook
-
     , modMask = mod4Mask
 
-    , borderWidth = 3
-
-    , normalBorderColor = "#586e75"
-
-    , focusedBorderColor = "#eee8d5"
+    , borderWidth = 0
 
     }
     `additionalKeysP`
