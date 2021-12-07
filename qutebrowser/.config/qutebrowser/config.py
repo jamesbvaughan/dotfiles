b83f3e1df1b3435b52a46d65df40c05a0fae5665
James Vaughan <james@jamesbvaughan.com>
2021-12-06 22:01:19 -0800

    Update qutebrowser config

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -4,21 +4,8 @@ import subprocess
 c = c  # noqa: F821 pylint: disable=E0602,C0103
 config = config  # noqa: F821 pylint: disable=E0602,C0103
 
-
-def read_xresources(prefix):
-    props = {}
-    x = subprocess.run(['xrdb', '-query'], stdout=subprocess.PIPE)
-    lines = x.stdout.decode().split('\n')
-    for line in filter(lambda l: l.startswith(prefix), lines):
-        prop, _, value = line.partition(':\t')
-        props[prop] = value
-    return props
-
-
-xresources = read_xresources('*')
-
-uiFont = "24px DejaVu Sans Mono"
-uiFontBold = "bold {}".format(uiFont)
+# uiFont = "10px JetBrains Mono Nerd Font"
+# uiFontBold = "bold {}".format(uiFont)
 
 config.bind("<Ctrl-W>", 'fake-key <Ctrl-Backspace>', mode='insert')
 config.bind("<Ctrl-A>", 'fake-key <Home>', mode='insert')
@@ -49,15 +36,17 @@ c.downloads.location.prompt = False
 c.downloads.position = "bottom"
 c.downloads.remove_finished = 2000
 
-c.editor.command = ["urxvt", "-e", "vim", "{}"]
+c.editor.command = ["alacritty", "-e", "nvim", "{}"]
 
-c.fonts.completion.entry = uiFont
-c.fonts.completion.category = uiFontBold
-c.fonts.downloads = uiFont
-c.fonts.tabs = uiFont
-c.fonts.statusbar = uiFont
-c.fonts.hints = uiFont
-c.fonts.keyhint = uiFont
+c.fonts.default_family = "JetBrains Mono Nerd Font"
+c.fonts.default_size = "10px"
+# c.fonts.completion.entry = uiFont
+# c.fonts.completion.category = uiFontBold
+# c.fonts.downloads = uiFont
+# # c.fonts.tabs = uiFont
+# c.fonts.statusbar = uiFont
+# c.fonts.hints = uiFont
+# c.fonts.keyhint = uiFont
 
 c.tabs.indicator.width = 1
 c.tabs.padding = {
@@ -67,84 +56,11 @@ c.tabs.padding = {
     'right': 1,
 }
 c.tabs.show = "multiple"
-c.tabs.title.format_pinned = ""
-c.tabs.title.format = "{title}"
+# c.tabs.title.format_pinned = ""
+# c.tabs.title.format = "{title}"
 
 c.spellcheck.languages = ['en-US']
 
-c.url.searchengines = {
-    'DEFAULT': 'https://duckduckgo.com/?q={}',
-    'g': 'https://google.com/search?q={}',
-    'y': 'https://youtube.com/results?search_query={}',
-    'a': 'https://smile.amazon.com/s?field-keywords={}',
-    'r': 'https://reddit.com/r/{}',
-    'm': 'https://www.reddit.com/me/m/{}'
-}
+c.zoom.default = '75%'
 
-c.zoom.default = '150%'
-
-c.colors.completion.category.bg = xresources['*background']
-c.colors.completion.category.border.bottom = xresources['*background']
-c.colors.completion.category.border.top = xresources['*background']
-c.colors.completion.category.fg = xresources['*color15']
-c.colors.completion.even.bg = xresources['*color0']
-c.colors.completion.fg = xresources['*color14']
-c.colors.completion.item.selected.bg = xresources['*color13']
-c.colors.completion.item.selected.border.bottom = xresources['*color13']
-c.colors.completion.item.selected.border.top = xresources['*color13']
-c.colors.completion.item.selected.fg = xresources['*color15']
-c.colors.completion.match.fg = xresources['*color7']
-c.colors.completion.odd.bg = xresources['*color0']
-c.colors.completion.scrollbar.bg = xresources['*color14']
-c.colors.completion.scrollbar.fg = xresources['*color7']
-c.colors.downloads.bar.bg = xresources['*background']
-c.colors.downloads.error.bg = xresources['*color1']
-c.colors.downloads.error.fg = xresources['*color15']
-c.colors.downloads.start.fg = xresources['*color15']
-c.colors.hints.bg = xresources['*color13']
-c.colors.hints.fg = xresources['*color15']
-c.colors.hints.match.fg = xresources['*color14']
-c.colors.keyhint.fg = xresources['*color15']
-c.colors.keyhint.suffix.fg = xresources['*color3']
-c.colors.messages.error.bg = xresources['*color1']
-c.colors.messages.error.border = xresources['*color1']
-c.colors.messages.error.fg = xresources['*color15']
-c.colors.messages.info.bg = xresources['*background']
-c.colors.messages.info.border = xresources['*background']
-c.colors.messages.info.fg = xresources['*color15']
-c.colors.messages.warning.bg = xresources['*color9']
-c.colors.messages.warning.border = xresources['*color9']
-c.colors.messages.warning.fg = xresources['*color15']
-c.colors.prompts.bg = xresources['*color0']
-c.colors.prompts.border = '1px solid ' + xresources['*color15']
-c.colors.prompts.fg = xresources['*color15']
-c.colors.prompts.selected.bg = xresources['*color10']
-c.colors.statusbar.caret.bg = xresources['*color4']
-c.colors.statusbar.caret.fg = xresources['*color14']
-c.colors.statusbar.caret.selection.bg = xresources['*color13']
-c.colors.statusbar.caret.selection.fg = xresources['*color14']
-c.colors.statusbar.command.bg = xresources['*background']
-c.colors.statusbar.command.fg = xresources['*color14']
-c.colors.statusbar.command.private.bg = xresources['*color10']
-c.colors.statusbar.command.private.fg = xresources['*color15']
-c.colors.statusbar.insert.bg = xresources['*color0']
-c.colors.statusbar.insert.fg = xresources['*color14']
-c.colors.statusbar.normal.bg = xresources['*background']
-c.colors.statusbar.normal.fg = xresources['*color14']
-c.colors.statusbar.private.bg = xresources['*color10']
-c.colors.statusbar.private.fg = xresources['*color15']
-c.colors.statusbar.progress.bg = xresources['*color14']
-c.colors.statusbar.url.error.fg = xresources['*color1']
-c.colors.statusbar.url.fg = xresources['*color14']
-c.colors.statusbar.url.hover.fg = xresources['*color7']
-c.colors.statusbar.url.success.http.fg = xresources['*color14']
-c.colors.statusbar.url.success.https.fg = xresources['*color14']
-c.colors.statusbar.url.warn.fg = xresources['*color3']
-c.colors.tabs.even.bg = xresources['*color0']
-c.colors.tabs.even.fg = xresources['*color14']
-c.colors.tabs.odd.bg = xresources['*background']
-c.colors.tabs.odd.fg = xresources['*color14']
-c.colors.tabs.selected.even.bg = xresources['*color14']
-c.colors.tabs.selected.even.fg = xresources['*color7']
-c.colors.tabs.selected.odd.bg = xresources['*color14']
-c.colors.tabs.selected.odd.fg = xresources['*color7']
+config.load_autoconfig(False)
