e80453dad9ee356c915562918908d580a95dc0af
James Vaughan <james@jamesbvaughan.com>
2021-12-11 15:18:42 -0800

    Update qutebrowser config

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,11 +1,16 @@
-import subprocess
+import dracula.draw
 
 # pylint: disable=C0111
 c = c  # noqa: F821 pylint: disable=E0602,C0103
 config = config  # noqa: F821 pylint: disable=E0602,C0103
 
-# uiFont = "10px JetBrains Mono Nerd Font"
-# uiFontBold = "bold {}".format(uiFont)
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
+
 
 config.bind("<Ctrl-W>", 'fake-key <Ctrl-Backspace>', mode='insert')
 config.bind("<Ctrl-A>", 'fake-key <Home>', mode='insert')
@@ -22,10 +27,6 @@ config.bind('<Alt-Shift-p>', 'spawn --userscript qute-pass --password-only')
 config.bind('sp', 'open -t https://getpocket.com/edit?url={url}')
 config.bind(';p', 'hint links run :open -p {hint-url}')
 
-c.aliases = {
-    'q': 'quit',
-}
-
 c.auto_save.session = True
 
 c.completion.scrollbar.width = 0
@@ -40,15 +41,15 @@ c.editor.command = ["alacritty", "-e", "nvim", "{}"]
 
 c.fonts.default_family = "JetBrains Mono Nerd Font"
 c.fonts.default_size = "10px"
-# c.fonts.completion.entry = uiFont
-# c.fonts.completion.category = uiFontBold
-# c.fonts.downloads = uiFont
-# # c.fonts.tabs = uiFont
-# c.fonts.statusbar = uiFont
-# c.fonts.hints = uiFont
-# c.fonts.keyhint = uiFont
 
-c.tabs.indicator.width = 1
+c.statusbar.padding = {
+    'top': 0,
+    'bottom': 0,
+    'left': 0,
+    'right': 0,
+}
+
+c.tabs.indicator.width = 0
 c.tabs.padding = {
     'top': 1,
     'bottom': 1,
@@ -57,10 +58,12 @@ c.tabs.padding = {
 }
 c.tabs.show = "multiple"
 # c.tabs.title.format_pinned = ""
-# c.tabs.title.format = "{title}"
+c.tabs.title.format = "{current_title}"
 
 c.spellcheck.languages = ['en-US']
 
 c.zoom.default = '75%'
+
+c.colors.webpage.preferred_color_scheme = 'dark'
 
 config.load_autoconfig(False)
