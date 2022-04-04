eec75df6041072be2f5a74dc433453122f0cdd52
James Vaughan <james@jamesbvaughan.com>
2022-04-04 14:11:50 -0700

    Rename "master" to "main"

--- bash/.bash.d/aliases.bash
+++ bash/.bash.d/aliases.bash
@@ -12,7 +12,7 @@ alias ls='exa --long --header'
 alias gb="git branch"
 alias gc="git commit"
 alias gcl="git clone"
-alias gcm="git checkout master"
+alias gcm="git checkout main"
 alias gco="git checkout"
 alias gd="git diff"
 alias gf="git fetch"
@@ -23,7 +23,7 @@ alias gpl="git pull"
 alias gs="git status"
 alias gx="git add . && git commit -m"
 alias gamp="git commit . --amend --no-edit && git push --force origin \$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)"
-alias prune="git branch --merged master | grep -v master | xargs -n 1 git branch -d"
+alias prune="git branch --merged main | grep -v main | xargs -n 1 git branch -d"
 alias stash="git stash"
 alias pop="git stash pop"
 alias pr="hub pull-request"
@@ -40,12 +40,12 @@ function hack() {
 }
 
 function bdel() {
-  local branch=$(git branch | grep -v '*' | grep -v 'master' | sed 's/^  //' | fzf --no-preview)
+  local branch=$(git branch | grep -v '*' | grep -v 'main' | sed 's/^  //' | fzf --no-preview)
   git branch -D $branch
 }
 
 function merge() {
-  local branch=$(git branch | grep -v '*' | grep -v 'master' | sed 's/^  //' | fzf --no-preview)
+  local branch=$(git branch | grep -v '*' | grep -v 'main' | sed 's/^  //' | fzf --no-preview)
   git stash \
     && git pull \
     && git merge --no-edit $branch \
