6d18f34006aa44ec8873320d9d66a8a10c5a83b7
James Vaughan <james@jamesbvaughan.com>
2022-04-15 15:42:17 -0700

    Update nvim statusline config

--- neovim/.config/nvim/lua/statusline.lua
+++ neovim/.config/nvim/lua/statusline.lua
@@ -1,44 +1,53 @@
 -- luacheck: globals vim
-local lsp_status = require('lsp-status')
-local lualine = require('lualine')
+local lsp_status = require("lsp-status")
+local lualine = require("lualine")
 
-lsp_status.config {
-  show_filename = true,
-}
+lsp_status.config({
+	show_filename = true,
+})
 
-lualine.setup {
-  options = {
-    theme = 'dracula-nvim',
-    section_separators = '',
-    component_separators = '|',
-    extensions = {
-      'quickfix',
-    },
-    globalstatus = true,
-  },
-  sections = {
-    lualine_b = {
-      'branch',
-      'diff',
-    },
-    lualine_c = {
-      {
-        'filename',
-        path = 1,
-      },
-    },
-    lualine_x = {
-      {
-        'diagnostics',
-        sources = {'nvim_diagnostic'}
-      },
-      'filetype',
-    },
-    lualine_y = {
-      'location',
-    },
-    lualine_z = {
-      lsp_status.status,
-    },
-  },
-}
+lualine.setup({
+	options = {
+		theme = "dracula-nvim",
+		section_separators = "",
+		component_separators = "",
+		globalstatus = true,
+	},
+	extensions = {
+		"quickfix",
+		"fugitive",
+	},
+	sections = {
+		lualine_a = {
+			{
+				"mode",
+				fmt = function(str)
+					return str:sub(1, 1)
+				end,
+			},
+		},
+		lualine_b = {
+			"branch",
+		},
+		lualine_c = {
+			"filename",
+			"diff",
+		},
+		lualine_x = {
+			{
+				"diagnostics",
+				sources = { "nvim_diagnostic" },
+			},
+		},
+		lualine_y = {
+			{
+				"filetype",
+				icon_only = true,
+			},
+			"location",
+		},
+		lualine_z = {
+			lsp_status.status,
+		},
+	},
+})
