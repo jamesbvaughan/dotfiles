cef02fcd8b9110a5a58d9b580f64ed1e1708f9b9
James Vaughan <jamesbvaughan@stripe.com>
2022-04-20 11:00:30 -0700

    fixup! Set up completion for more things

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -132,6 +132,7 @@ require("packer").startup(function(use)
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-nvim-lua",
 			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
 			"onsails/lspkind-nvim",
 		},
 	})
