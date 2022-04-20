742d8d34a53bcf07327d54e306f45d247412aae8
James Vaughan <jamesbvaughan@stripe.com>
2022-04-20 10:58:46 -0700

    Set up completion for more things

--- neovim/.config/nvim/lua/completion.lua
+++ neovim/.config/nvim/lua/completion.lua
@@ -75,5 +75,32 @@ cmp.setup {
   },
 }
 
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/`
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':'
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
 -- Use nvim-autopairs to insert parens after completing a function name
 cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
