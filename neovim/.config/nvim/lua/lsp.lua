059a076139421607efcdf28f0d2e8279d12016df
James Vaughan <jamesbvaughan@stripe.com>
2022-04-20 11:18:18 -0700

    Add comment about signs

--- neovim/.config/nvim/lua/lsp.lua
+++ neovim/.config/nvim/lua/lsp.lua
@@ -164,13 +164,13 @@ null_ls.setup({
 	},
 })
 
+-- Configure symbols for diagnostic signs
 local signs = {
 	Error = " ",
 	Warn = " ",
 	Hint = " ",
 	Info = " ",
 }
-
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
