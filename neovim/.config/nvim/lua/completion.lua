0574d81cf77910a0ea9d7d1317c8c5233cfc8e3b
James Vaughan <jamesbvaughan@stripe.com>
2022-04-20 10:58:17 -0700

    Abort rather than just close on C-e when completing

--- neovim/.config/nvim/lua/completion.lua
+++ neovim/.config/nvim/lua/completion.lua
@@ -40,7 +40,7 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
+    ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
