319066f07a6d65986a923c36511afe379793b7bc
James Vaughan <james@jamesbvaughan.com>
2022-05-13 09:17:51 -0500

    Format lsp.lua

--- neovim/.config/nvim/lua/lsp.lua
+++ neovim/.config/nvim/lua/lsp.lua
@@ -5,45 +5,45 @@ local cmp_nvim_lsp = require("cmp_nvim_lsp")
 -- vim.lsp.set_log_level('debug')
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	virtual_text = false,
+  virtual_text = false,
   float = {
     source = "always",
   },
-	signs = true,
-	underline = true,
-	update_in_insert = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
 })
 
-local on_attach = function(client, bufnr)
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+local on_attach = function(_client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	local function bufmap(combo, mapping)
-		local opts = { noremap = true, silent = true }
-		vim.api.nvim_buf_set_keymap(bufnr, "n", combo, mapping, opts)
-	end
+  local function bufmap(combo, mapping)
+    local opts = { noremap = true, silent = true }
+    vim.api.nvim_buf_set_keymap(bufnr, "n", combo, mapping, opts)
+  end
 
-	bufmap("gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-	bufmap("gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-	bufmap("K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-	bufmap("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-	-- bufmap('<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-	bufmap("<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-	bufmap("<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-	bufmap("gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-	bufmap("<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-	-- bufmap('<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>')
-	bufmap("<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
-	bufmap("[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
-	bufmap("]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
-	bufmap("<leader>q", ":TroubleToggle<CR>")
+  bufmap("gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+  bufmap("gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+  bufmap("K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+  bufmap("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+  -- bufmap('<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+  bufmap("<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  bufmap("<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+  bufmap("gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+  bufmap("<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+  -- bufmap('<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>')
+  bufmap("<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+  bufmap("[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
+  bufmap("]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
+  bufmap("<leader>q", ":TroubleToggle<CR>")
 
   -- Formatting
-	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
-	vim.cmd([[ command! FormatSync execute 'lua vim.lsp.buf.formatting_seq_sync()' ]])
-	bufmap("<leader>f", ":FormatSync<CR>")
+  vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
+  vim.cmd([[ command! FormatSync execute 'lua vim.lsp.buf.formatting_seq_sync()' ]])
+  bufmap("<leader>f", ":FormatSync<CR>")
 
-	-- Format on save
-	-- vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]])
+  -- Format on save
+  -- vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]])
 end
 
 -- nvim-cmp supports additional completion capabilities
@@ -52,20 +52,20 @@ capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 -- set the on_attach callback and capabilities for all servers
 lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
-	capabilities = capabilities,
-	on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
 })
 
 -- Enable the following language servers
 lspconfig.bashls.setup({})
 lspconfig.cssls.setup({
-	settings = {
-		scss = {
-			lint = {
-				unknownAtRules = "ignore",
-			},
-		},
-	},
+  settings = {
+    scss = {
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+  },
 })
 lspconfig.dartls.setup({})
 -- lspconfig.denols.setup({})
@@ -120,7 +120,7 @@ require 'lspconfig'.sumneko_lua.setup {
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
+        globals = { 'vim' },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
@@ -133,32 +133,31 @@ require 'lspconfig'.sumneko_lua.setup {
 -- Use null-ls for general linters
 local null_ls = require("null-ls")
 null_ls.setup({
-	on_attach = on_attach,
-	sources = {
-		null_ls.builtins.diagnostics.rubocop,
-		null_ls.builtins.diagnostics.shellcheck,
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.rubocop,
-		null_ls.builtins.formatting.shfmt,
-	},
+  on_attach = on_attach,
+  sources = {
+    null_ls.builtins.diagnostics.rubocop,
+    null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.rubocop,
+    null_ls.builtins.formatting.shfmt,
+  },
 })
 
 -- Configure symbols for diagnostic signs
 local signs = {
-	Error = " ",
-	Warn = " ",
-	Hint = " ",
-	Info = " ",
+  Error = " ",
+  Warn = " ",
+  Hint = " ",
+  Info = " ",
 }
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 -- Show line diagnostics in hover window
 vim.o.updatetime = 500
 vim.api.nvim_create_autocmd("CursorHold", {
-  buffer = bufnr,
   callback = function()
     local opts = {
       focusable = false,
