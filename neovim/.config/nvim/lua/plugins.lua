82877c390fb4849ca7cddf2bad696067b0aa5011
James Vaughan <james@jamesbvaughan.com>
2022-07-21 13:36:59 -0700

    Fix bufferline config

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -335,7 +335,7 @@ require("packer").startup(function(use)
           show_buffer_close_icons = false,
           show_close_icon = false,
           always_show_bufferline = false,
-          diagnostics = "nvim_lsp",
+          diagnostics = true,
         },
       })
     end,
