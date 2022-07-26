645e9f3048cce2ca2b4b461c1fd381df195ebbaa
James Vaughan <james@jamesbvaughan.com>
2022-07-25 20:36:34 -0800

    Fix find files keybinding

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -238,7 +238,7 @@ require("packer").startup(function(use)
 
       local builtins = require("telescope.builtin")
 
-      vim.keymap.set("n", "<leader>fa", builtins.find_files)
+      vim.keymap.set("n", "<leader>ff", builtins.find_files)
       vim.keymap.set("n", "<leader>fg", builtins.live_grep)
       vim.keymap.set("n", "<leader>fb", builtins.buffers)
       vim.keymap.set("n", "<leader>fa", builtins.git_files)
