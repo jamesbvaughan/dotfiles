506c2a61fffd340faa8a1e13057ded9d0f3af0e3
James Vaughan <james@jamesbvaughan.com>
2023-03-15 14:05:53 -0700

    ???

--- neovim/.config/nvim/lua/null_ls.lua
+++ neovim/.config/nvim/lua/null_ls.lua
@@ -6,12 +6,12 @@ local null_opts = lsp.build_options('null-ls', {})
 null_ls.setup({
   on_attach = null_opts.on_attach,
   sources = {
-    null_ls.builtins.diagnostics.rubocop,
+    -- null_ls.builtins.diagnostics.rubocop,
     null_ls.builtins.diagnostics.shellcheck,
     null_ls.builtins.diagnostics.vale,
     null_ls.builtins.formatting.autopep8,
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.rubocop,
+    -- null_ls.builtins.formatting.rubocop,
     null_ls.builtins.formatting.shfmt,
     null_ls.builtins.formatting.joker,
     require("typescript.extensions.null-ls.code-actions"),
