a7f91e016e867064b5aa5ce79d386ee13ca35ce7
James Vaughan <james@jamesbvaughan.com>
2023-11-30 16:06:30 -0800

    Switch back to gruvbox and simplify config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,6 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.font = wezterm.font 'Berkeley Mono'
--- config.font = wezterm.font 'Monaspace Argon'
 config.font_size = 16
 
 config.hide_tab_bar_if_only_one_tab = true
@@ -10,21 +9,8 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 40
 
-local function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
-  end
-  return 'Dark'
-end
-
-local function scheme_for_appearance(appearance)
-  if appearance:find 'Dark' then
-    return 'flexoki-dark'
-  else
-    return 'flexoki-light'
-  end
-end
-
-config.color_scheme = scheme_for_appearance(get_appearance())
+config.color_scheme = wezterm.gui.get_appearance():find 'Dark'
+  and 'GruvboxDarkHard'
+  or 'Gruvbox Light'
 
 return config
