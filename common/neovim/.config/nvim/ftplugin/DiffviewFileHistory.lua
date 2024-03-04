ee90a512f9ff57b5c702acd86b408f1525b71aeb
James Vaughan <james@jamesbvaughan.com>
2024-03-03 17:53:15 -0800

    Format

--- common/neovim/.config/nvim/ftplugin/DiffviewFileHistory.lua
+++ common/neovim/.config/nvim/ftplugin/DiffviewFileHistory.lua
@@ -1,6 +1,1 @@
-vim.keymap.set(
-  'n',
-  'q',
-  vim.cmd.DiffviewClose,
-  { desc = "Close diffview", buffer = true }
-)
+vim.keymap.set("n", "q", vim.cmd.DiffviewClose, { desc = "Close diffview", buffer = true })
