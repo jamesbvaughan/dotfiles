111a97ecce6dec4de08d49adb32f6b3f5d1ae85f
James Vaughan <james@jamesbvaughan.com>
2024-05-18 13:19:36 -0700

    Update neogit config

--- common/neovim/.config/nvim/lua/plugins/git.lua
+++ common/neovim/.config/nvim/lua/plugins/git.lua
@@ -1,59 +1,67 @@
 return {
-  {
-    "tpope/vim-fugitive",
-    lazy = false,
-    dependencies = "tpope/vim-rhubarb",
-    keys = {
-      { "gh", vim.cmd.GBrowse,                                               desc = "Open the current file in GitHub" },
-      { "gl", ":Git log --pretty --oneline --abbrev-commit --graph -20 <cr>" },
-    }
-  },
+	{
+		"tpope/vim-fugitive",
+		lazy = false,
+		dependencies = "tpope/vim-rhubarb",
+		keys = {
+			{
+				"gh",
+				vim.cmd.GBrowse,
+				desc = "Open the current file in GitHub",
+			},
+			{ "gl", ":Git log --pretty --oneline --abbrev-commit --graph -20 <cr>" },
+		},
+	},
 
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      current_line_blame = true,
-      current_line_blame_opts = {
-        ignore_whitespace = true,
-        delay = 500,
-      },
-    },
-  },
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			current_line_blame = true,
+			current_line_blame_opts = {
+				ignore_whitespace = true,
+				delay = 500,
+			},
+		},
+	},
 
-  {
-    'NeogitOrg/neogit',
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    keys = {
-      { "<leader>g", vim.cmd.Neogit, desc = "Open neogit" },
-    },
-    opts = {
-      disable_hint = true,
-      disable_insert_on_commit = false,
-      integrations = {
-        diffview = true,
-        telescope = true,
-      },
-      graph_style = "unicode",
-      telescope_sorter = function()
-        return require("telescope").extensions.fzf.native_fzf_sorter()
-      end,
-    }
-  },
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		keys = {
+			{ "<leader>g", vim.cmd.Neogit, desc = "Open neogit" },
+		},
+		opts = {
+			disable_hint = true,
+			disable_insert_on_commit = false,
+			graph_style = "unicode",
+			signs = {
+				-- { CLOSED, OPENED }
+				hunk = { "", "" },
+				item = { "▶", "▼" },
+				section = { "▶", "▼" },
+			},
+			telescope_sorter = function()
+				return require("telescope").extensions.fzf.native_fzf_sorter()
+			end,
+		},
+	},
 
-  {
-    'sindrets/diffview.nvim',
-    keys = {
-      {
-        "<leader>h",
-        function() vim.cmd.DiffviewFileHistory("%") end,
-        desc = "Open diffview for the current file"
-      },
-    },
-    opts = {
-      enhanced_diff_hl = true,
-    },
-  },
+	{
+		"sindrets/diffview.nvim",
+		keys = {
+			{
+				"<leader>h",
+				function()
+					vim.cmd.DiffviewFileHistory("%")
+				end,
+				desc = "Open diffview for the current file",
+			},
+		},
+		opts = {
+			enhanced_diff_hl = true,
+		},
+	},
 }
