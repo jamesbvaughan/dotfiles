1a21f0b7126c7adf73d869444506f04a80a492f0
James Vaughan <james@jamesbvaughan.com>
2024-05-18 13:20:31 -0700

    Update plugin config

--- common/neovim/.config/nvim/lua/plugins-lazy.lua
+++ common/neovim/.config/nvim/lua/plugins-lazy.lua
@@ -1,79 +1,80 @@
 -- Bootstrap lazy.nvim
 --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not vim.uv.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-
 
 -- Install plugins
 --
 require("lazy").setup({
-  { import = "plugins" },
+	{ import = "plugins" },
 
-  "tpope/vim-surround",
+	"tpope/vim-surround",
 
-  "tpope/vim-repeat",
+	"tpope/vim-repeat",
 
-  -- Better integration between nvim and tmux
-  {
-    "christoomey/vim-tmux-navigator",
-    keys = {
-      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
+	-- Better integration between nvim and tmux
+	{
+		"christoomey/vim-tmux-navigator",
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
 
-  "windwp/nvim-autopairs",
+	"windwp/nvim-autopairs",
 
-  {
-    "mbbill/undotree",
-    keys = {
-      { "U", vim.cmd.UndotreeToggle },
-    },
-  },
+	{
+		"mbbill/undotree",
+		keys = {
+			{ "U", vim.cmd.UndotreeToggle },
+		},
+	},
 
-  -- quick navigation within a file
-  {
-    'ggandor/leap.nvim',
-    config = function()
-      require('leap').set_default_keymaps()
+	-- quick navigation within a file
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").set_default_keymaps()
 
-      -- Grey out the search area
-      vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
-    end,
-  },
+			-- Grey out the search area
+			vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+		end,
+	},
 
-  -- nice bindings for working with comments
-  {
-    'numToStr/Comment.nvim',
-    event = { "InsertEnter" },
-    dependencies = {
-      'JoosepAlviste/nvim-ts-context-commentstring',
-    },
-    config = function()
-      require('Comment').setup({
-        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-      })
-    end,
-  },
+	-- nice bindings for working with comments
+	-- This is built into neovim by default now. The only reason I'm keeping this
+	-- plugin around is for commenting jsx
+	{
+		"numToStr/Comment.nvim",
+		event = { "InsertEnter" },
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		config = function()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
+	},
 }, {
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
