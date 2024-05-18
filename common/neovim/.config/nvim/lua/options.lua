e3dd7af7d95f4a0e5e19a867f082e3961c9d89f4
James Vaughan <james@jamesbvaughan.com>
2024-05-18 13:20:04 -0700

    Update colorcolumn config

--- common/neovim/.config/nvim/lua/options.lua
+++ common/neovim/.config/nvim/lua/options.lua
@@ -1,5 +1,5 @@
 --- use space as the leader key
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 ---- don't use swap files
 vim.opt.swapfile = false
@@ -28,10 +28,10 @@ vim.opt.cursorline = true
 vim.opt.autoread = true
 
 ---- enable the mouse
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 ---- use the system clipboard
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
 ---- start scrolling before hitting the bottom
 vim.opt.scrolloff = 5
@@ -40,27 +40,28 @@ vim.opt.scrolloff = 5
 -- vim.opt.splitkeep = 'screen'
 
 ---- ignore these when autocompleting paths
-vim.opt.wildignore = vim.opt.wildignore + 'node_modules/*,vendor/bundle/*,tmp/*'
+vim.opt.wildignore = vim.opt.wildignore + "node_modules/*,vendor/bundle/*,tmp/*"
 
 ---- only use case sensitive search when uppercase
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 ---- persistent undo files
-vim.opt.undodir = vim.fn.getenv('HOME') .. '/.local/nvim/undofiles'
+vim.opt.undodir = vim.fn.getenv("HOME") .. "/.local/nvim/undofiles"
 vim.opt.undofile = true
 
 ---- always show the sign column
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 ---- always show the sign column
-vim.opt.colorcolumn = '80'
+vim.opt.textwidth = 80
+vim.opt.colorcolumn = "+1"
 
 --- enable 24-bit color
 vim.opt.termguicolors = true
 
 ---- configure the spell file
-vim.opt.spellfile = vim.fn.getenv('HOME') .. '/.config/nvim/spell/en.utf-8.add'
+vim.opt.spellfile = vim.fn.getenv("HOME") .. "/.config/nvim/spell/en.utf-8.add"
 
 ---- don't keep search results highlighted
 vim.opt.hlsearch = false
