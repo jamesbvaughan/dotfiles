90140c438406113c6d36efa577591fe97a56dbd5
James Vaughan <james@jamesbvaughan.com>
2024-08-05 16:19:35 -0700

    Setup auto-dark-mode

--- common/neovim/.config/nvim/lua/plugins/colors.lua
+++ common/neovim/.config/nvim/lua/plugins/colors.lua
@@ -67,5 +67,16 @@ return {
 	{
 		"f-person/auto-dark-mode.nvim",
 		lazy = false,
+		opts = {
+			update_interval = 1000,
+			set_dark_mode = function()
+				vim.api.nvim_set_option_value("background", "dark", {})
+				vim.cmd("colorscheme catppuccin")
+			end,
+			set_light_mode = function()
+				vim.api.nvim_set_option_value("background", "light", {})
+				vim.cmd("colorscheme catppuccin")
+			end,
+		},
 	},
 }
