f5cdf55813887403597462dc1ae9d1f55d9f3bac
James Vaughan <james@jamesbvaughan.com>
2024-11-18 11:05:12 -0800

    Update wezterm config

--- common/wezterm/.config/wezterm/wezterm.lua
+++ common/wezterm/.config/wezterm/wezterm.lua
@@ -1,38 +1,27 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Font
-config.font = wezterm.font 'Berkeley Mono'
+config.font = wezterm.font("Berkeley Mono")
 config.font_size = 12
 
 -- Tab bar
-config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
 -- Window
--- config.window_background_opacity = 0.8
--- config.macos_window_background_blur = 40
 config.window_padding = {
-  left = '1px',
-  right = '1px',
-  top = '1px',
-  bottom = '1px',
+	left = "0px",
+	right = "0px",
+	top = "0px",
+	bottom = "0px",
 }
 
 -- Colors
---- Set up a version of gruvbox with a pure black background
-local color_schemes = wezterm.color.get_builtin_schemes()
-local gruvbox_dark_extra_hard = color_schemes['GruvboxDarkHard']
-gruvbox_dark_extra_hard.background = '#000'
-gruvbox_dark_extra_hard.tab_bar = { background = '#000' }
-config.color_schemes = { ['GruvboxDarkExtraHard'] = gruvbox_dark_extra_hard }
-
---- Set the colorschemes
-local dark_mode_theme = 'GruvboxDarkExtraHard'
-local light_mode_theme = 'Gruvbox Light'
+local dark_mode_theme = "Catppuccin Mocha"
+local light_mode_theme = "Catppuccin Latte"
 
 --- Follow the system dark/light mode
-local dark_mode = wezterm.gui.get_appearance():find 'Dark'
+local dark_mode = wezterm.gui.get_appearance():find("Dark")
 config.color_scheme = dark_mode and dark_mode_theme or light_mode_theme
 
 return config
