a6cc6cc243845d70e8f4c5c31ddd8a5ab385a7f3
James Vaughan <james@jamesbvaughan.com>
2024-12-29 16:59:04 -0800

    Remove archive

--- archive/bash/.bash.d/colored-man-pages.bash
+++
@@ -1,9 +0,0 @@
-man() {
-  LESS_TERMCAP_md=$'\e[01;31m' \
-  LESS_TERMCAP_me=$'\e[0m' \
-  LESS_TERMCAP_se=$'\e[0m' \
-  LESS_TERMCAP_so=$'\e[01;44;33m' \
-  LESS_TERMCAP_ue=$'\e[0m' \
-  LESS_TERMCAP_us=$'\e[01;32m' \
-  command man "$@"
-}
