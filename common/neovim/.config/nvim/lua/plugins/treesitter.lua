f3594638a446d207424daeda59c55493f85abf27
James Vaughan <james@jamesbvaughan.com>
2025-01-17 22:45:51 -0800

    Add ghostty plugin

--- common/neovim/.config/nvim/lua/plugins/treesitter.lua
+++ common/neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,11 @@
 return {
 	{
+		"bezhermoso/tree-sitter-ghostty",
+		build = "make nvim_install",
+		lazy = false,
+	},
+
+	{
 		"nvim-treesitter/nvim-treesitter",
 		dependencies = {
 			-- Additional text objects
