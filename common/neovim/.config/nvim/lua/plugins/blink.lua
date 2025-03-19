293633abe52766ae7591f627174ba14350dc7937
James Vaughan <james@jamesbvaughan.com>
2025-03-18 21:01:08 -0700

    Simplify cmp config

--- common/neovim/.config/nvim/lua/plugins/blink.lua
+++ common/neovim/.config/nvim/lua/plugins/blink.lua
@@ -5,7 +5,7 @@ return {
 	"saghen/blink.cmp",
 	dependencies = {
 		"rafamadriz/friendly-snippets",
-		"giuxtaposition/blink-cmp-copilot",
+		"fang2hou/blink-copilot",
 
 		-- github copilot
 		{
@@ -33,56 +33,23 @@ return {
 		},
 
 		completion = {
-			menu = {
-				draw = {
-					columns = {
-						{ "kind_icon", "label", gap = 1 },
-					},
-					treesitter = { "lsp" },
-					components = {
-						label = {
-							width = {
-								-- Shorten the max label width since copilot labels can be
-								-- really long.
-								max = 30,
-							},
-						},
-					},
-				},
-				auto_show = function(ctx)
-					return ctx.mode ~= "cmdline" or not vim.tbl_contains({ "/", "?" }, vim.fn.getcmdtype())
-				end,
-			},
 			ghost_text = {
 				enabled = true,
 			},
 			documentation = {
 				auto_show = true,
-				auto_show_delay_ms = 0,
+				auto_show_delay_ms = 500,
 			},
 		},
 
 		sources = {
 			providers = {
 				copilot = {
-					name = "copilot",
-					module = "blink-cmp-copilot",
+					module = "blink-copilot",
 					score_offset = 100,
 					async = true,
-
-					-- This is only necessary to get item labels
-					transform_items = function(_, items)
-						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-						local kind_idx = #CompletionItemKind + 1
-						CompletionItemKind[kind_idx] = "Copilot"
-						for _, item in ipairs(items) do
-							item.kind = kind_idx
-						end
-						return items
-					end,
 				},
 				lazydev = {
-					name = "LazyDev",
 					module = "lazydev.integrations.blink",
 					-- make lazydev completions top priority (see `:h blink.cmp`)
 					score_offset = 100,
@@ -99,11 +66,5 @@ return {
 		},
 
 		signature = { enabled = true },
-
-		appearance = {
-			kind_icons = {
-				Copilot = "",
-			},
-		},
 	},
 }
