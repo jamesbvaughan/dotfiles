808e25299f49e853c5f8b5fb0fdf6b35e572f554
James Vaughan <james@jamesbvaughan.com>
2025-09-04 22:15:41 -0700

    Add keybinding for closing diffview with q

--- common/neovim/.config/nvim/lua/plugins/diffview.lua
+++ common/neovim/.config/nvim/lua/plugins/diffview.lua
@@ -13,5 +13,16 @@ return {
 	---@type DiffviewConfig
 	opts = {
 		enhanced_diff_hl = true,
+		keymaps = {
+			-- view = {
+			--   { "n", "q", actions.close, { desc = "Close help menu" } },
+			-- },
+			file_panel = {
+				{ "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+			},
+			file_history_panel = {
+				{ "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+			},
+		},
 	},
 }
