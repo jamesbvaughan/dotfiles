47e8cb7bd5133a6716547841d9040f07dcbd8526
James Vaughan <james@jamesbvaughan.com>
2026-08-13 12:37:19 -0700

    Disable snack images

--- common/neovim/.config/nvim/plugin/00-snacks.lua
+++ common/neovim/.config/nvim/plugin/00-snacks.lua
@@ -7,40 +7,47 @@ local Snacks = require("snacks")
 
 ---@type snacks.Config
 Snacks.setup({
-	bigfile = { enabled = true },
-	bufdelete = { enabled = true },
-	dim = { enabled = true },
-	gh = {
-		enabled = true,
-		wo = {
-			spell = false,
-		},
-	},
-	gitbrowse = { enabled = true },
-	image = { enabled = true },
-	indent = { enabled = true, animate = { enabled = false } },
-	input = { enabled = true },
-	notifier = { enabled = true },
-	picker = {
-		enabled = true,
-		ui_select = true,
-		win = {
-			input = {
-				keys = {
-					["<esc>"] = {
-						---@diagnostic disable-next-line: assign-type-mismatch
-						function(picker)
-							picker:close()
-						end,
-						mode = { "n", "i" },
-					},
-				},
-			},
-		},
-	},
-	quickfile = { enabled = true },
-	statuscolumn = { enabled = true },
-	toggle = { enabled = true },
+  bigfile = { enabled = true },
+  bufdelete = { enabled = true },
+  dim = { enabled = true },
+  gh = {
+    enabled = true,
+    wo = {
+      spell = false,
+    },
+  },
+  gitbrowse = { enabled = true },
+  image = {
+    enabled = true,
+    doc = {
+      -- Ghostty doesn't support Kitty placement in code buffers/floats
+      -- https://github.com/ghostty-org/ghostty/issues/8272
+      enabled = false
+    },
+  },
+  indent = { enabled = true, animate = { enabled = false } },
+  input = { enabled = true },
+  notifier = { enabled = true },
+  picker = {
+    enabled = true,
+    ui_select = true,
+    win = {
+      input = {
+        keys = {
+          ["<esc>"] = {
+            ---@diagnostic disable-next-line: assign-type-mismatch
+            function(picker)
+              picker:close()
+            end,
+            mode = { "n", "i" },
+          },
+        },
+      },
+    },
+  },
+  quickfile = { enabled = true },
+  statuscolumn = { enabled = true },
+  toggle = { enabled = true },
 })
 
 -- Toggles
@@ -52,19 +59,19 @@ Snacks.toggle.dim():map("<leader>td")
 
 -- Keymaps
 local function pick(name, opts)
-	return function()
-		Snacks.picker(name, opts)
-	end
+  return function()
+    Snacks.picker(name, opts)
+  end
 end
 
 vim.keymap.set("n", "<leader>d", function()
-	Snacks.bufdelete()
+  Snacks.bufdelete()
 end, { desc = "Close the current buffer" })
 vim.keymap.set("n", "<leader>n", function()
-	Snacks.notifier.show_history()
+  Snacks.notifier.show_history()
 end, { desc = "Show notification history" })
 vim.keymap.set("n", "gh", function()
-	Snacks.gitbrowse.open()
+  Snacks.gitbrowse.open()
 end, { desc = "Open the current file on the web" })
 vim.keymap.set("n", "<leader>ff", pick("files"), { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", pick("grep", { hidden = true }), { desc = "Live grep" })
@@ -74,10 +81,10 @@ vim.keymap.set("n", "<leader><leader>", pick("smart"), { desc = "Smart file/buff
 vim.keymap.set("n", "<leader>fs", pick("grep_word"), { desc = "Search for the word under the cursor" })
 vim.keymap.set("n", "<leader>ft", pick("treesitter"), { desc = "Search treesitter symbols" })
 vim.keymap.set(
-	"n",
-	"<leader>fc",
-	pick("git_files", { cwd = "~/.dotfiles", untracked = true }),
-	{ desc = "Search dotfiles repo" }
+  "n",
+  "<leader>fc",
+  pick("git_files", { cwd = "~/.dotfiles", untracked = true }),
+  { desc = "Search dotfiles repo" }
 )
 -- Disabling this for now because it causes a delay before <leader>g opens Neogit
 -- vim.keymap.set("n", "<leader>gp", pick("gh_pr"), { desc = "Pull Requests" })
